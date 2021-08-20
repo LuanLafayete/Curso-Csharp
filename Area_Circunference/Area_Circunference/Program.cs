62502f51cf8fa4d1db8a22285bb7a518a7397c81
Luã Lafayete Salgado <78837159+LuanLafayete@users.noreply.github.com>
2021-08-20 16:33:56 -0300

    Calculando área de uma circunferência.
    O usuário apresenta os valores do RAIO e o sistema calcula:
    PI (3,14159) * potência do raio
    
    No final apresenta ao usuário o valor da Área.

---
+++ Area_Circunference/Area_Circunference/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+            //Atividade prática para calcular a área de uma circunferência. 
+            //O usuário apresenta os dados e o programa faz o calculo usando PI x a potência do dado apresentado pelo usuário. 
+        {
+            double n = 3.14159, area, raio;
+
+            raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            area = n * Math.Pow(raio, 2);
+
+            Console.WriteLine("Area = " + area.ToString("F4"), CultureInfo.InvariantCulture);
+
+        }
+    }
+}

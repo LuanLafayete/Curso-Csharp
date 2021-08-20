f3ad1a3251315d875d5c35693628677b2d6d035f
Luã Lafayete Salgado <78837159+LuanLafayete@users.noreply.github.com>
2021-08-20 16:13:46 -0300

    Area-perimeter-diagonal
    This is a test to practice C#.
    In this test it has a maths to calculate AREA / PERIMETER / DIAGONAL.
    
    The program ask the size of Hight and Width

---
+++ area-perimetro-diagonal/area-perimetro-diagonal/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace area_perimetro_diagonal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double b, a, area, perimetro, diagonal;
+
+            a = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            b = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            area = b * a;
+            perimetro = 2 * b + 2 * a;
+            diagonal = Math.Sqrt(Math.Pow(b, 2.0) + Math.Pow(a, 2.0));
+
+            Console.WriteLine("AREA = " + area.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("PERIMETRO = " + perimetro.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("DIAGONAL = " + diagonal.ToString("F4", CultureInfo.InvariantCulture));
+        }
+    }
+}

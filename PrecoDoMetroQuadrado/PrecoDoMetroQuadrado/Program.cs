407c981c0632684d91983b210e39530011fa85cf
Luã Lafayete Salgado <78837159+LuanLafayete@users.noreply.github.com>
2021-08-20 16:44:08 -0300

    Preço do Metro Quadrado
    Esse programa é um exercício para Corretoras ou Arquitetos.
    
    Esse sistema consiste em realizar o preço do metro quadrado.
    O usuário passa o tamanho da largura e o do comprimento. O sistema calcula a área e por fim há o calculo da área vezes o preço do metro quadrado.
    
    O sistema mostra o valor da área na primeira linha.
    Na segunda linha mostra o preço total do metro quadrado.

---
+++ PrecoDoMetroQuadrado/PrecoDoMetroQuadrado/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double largura, comprimento, precoMetroQuadrado, area, precoFinal;
+
+            Console.Write("Digite o tamanho da Largura: ");
+            largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Digite o tamanho do Comprimento: ");
+            comprimento = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Digite o Preço do Metro Quadrado: ");
+            precoMetroQuadrado = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            area = largura * comprimento;
+            precoFinal = area * precoMetroQuadrado;
+
+            Console.WriteLine("AREA = " + area.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("PREÇO = " + precoFinal.ToString("F2", CultureInfo.InvariantCulture));
+
+            Console.ReadLine();
+
+        }
+    }
+}

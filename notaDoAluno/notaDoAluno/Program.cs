427cf68bb20a4841f95daeb72de8fc0519855b89
Luã Lafayete Salgado <78837159+LuanLafayete@users.noreply.github.com>
2021-08-23 16:56:39 -0300

    APROVADO ou REPROVADO
    Exercício para desenvolver um sistema que faz a soma dos resultados dos testes, e envia a mensagem ao aluno de aprovado ou reprovado.
    
    O sistema solicita o resultado da primeira prova e o resultado da segunda prova;
    O sistema realiza a soma entre os dois resultado;
    Usando um condição composta, caso o alunos tenha uma nota maior ou igual a 60.00, o sistema envia a mensagem de APROVADO;
    Caso o aluno tenha uma nota menor que 60.00, o sistema envia a mensagem de REPROVADO

---
+++ notaDoAluno/notaDoAluno/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace notaDoAluno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double score1, score2, total;
+
+            Console.Write("Resuldado da primeira prova: ");
+            score1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Resuldado da segunda prova: ");
+            score2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            total = score1 + score2;
+
+            if (total >= 60.00)
+            {
+                Console.WriteLine("Seu resultado foi: " + total.ToString("f2"),CultureInfo.InvariantCulture);
+                Console.WriteLine("APROVADO");
+            }
+            else
+            {
+                Console.WriteLine("Seu resultado foi: " + total.ToString("f2"),CultureInfo.InvariantCulture);
+                Console.WriteLine("REPROVADO");
+            }
+
+
+        }
+    }
+}

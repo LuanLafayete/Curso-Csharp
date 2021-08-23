2e57c3154d7b6cb9ccddaaadc7928f23bc026da1
Luã Lafayete Salgado <78837159+LuanLafayete@users.noreply.github.com>
2021-08-23 12:21:08 -0300

    calculateSphere
    Exercício disponível no URI ONLINE JUDGE.
    
    Faça um programa que calcule e mostre o volume de uma esfera sendo fornecido o valor de seu raio (R). A fórmula para calcular o volume é: (4/3) * pi * R3. Considere (atribua) para pi o valor 3.14159.
    
    Dica: Ao utilizar a fórmula, procure usar (4/3.0) ou (4.0/3), pois algumas linguagens (dentre elas o C++), assumem que o resultado da divisão entre dois inteiros é outro inteiro.
    
    Entrada
    O arquivo de entrada contém um valor de ponto flutuante (dupla precisão), correspondente ao raio da esfera.
    
    Saída
    A saída deverá ser uma mensagem "VOLUME" conforme o exemplo fornecido abaixo, com um espaço antes e um espaço depois da igualdade. O valor deverá ser apresentado com 3 casas após o ponto.

---
+++ calculateSphere/calculateSphere/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace Esfera
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double pi = 3.14159, esfera;
+            int raio;
+
+            raio = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            esfera = (double)(4 / 3.0) * pi * Math.Pow(raio, 3.0);
+
+            Console.WriteLine("VOLUME = " + esfera.ToString("F3"), CultureInfo.InvariantCulture);
+
+
+        }
+    }
+}

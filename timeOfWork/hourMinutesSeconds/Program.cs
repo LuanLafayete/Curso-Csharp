0f7e204aab229df3ed38a2706b4f726194c61683
Luã Lafayete Salgado <78837159+LuanLafayete@users.noreply.github.com>
2021-08-23 13:43:25 -0300

    Calculate time of work (Seconds)
    This program receive the time of work and calculate in HOUR : MINUTES : SECONDS.
    
    The program ask your time of work, the user give the number and the program calculate and give the information to the user.

---
+++ timeOfWork/hourMinutesSeconds/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace hourMinutesSeconds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time, hour, resto01, resto02, minutes;
+
+            Console.Write("How many minutes did you work in this project? ");
+            time = int.Parse(Console.ReadLine());
+
+            hour = time / 3600;
+            resto01 = time % 3600;
+
+            minutes = resto01 / 60;
+            resto02 = resto01 % 60;
+
+           
+            Console.WriteLine("I worked: " + hour + ":" + minutes + ":" + resto02);
+
+
+        }
+    }
+}

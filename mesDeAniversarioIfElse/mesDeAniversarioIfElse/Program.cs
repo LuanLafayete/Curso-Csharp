e94107d92b96a20053a1804207ffdb66d895afe6
Luã Lafayete Salgado <78837159+LuanLafayete@users.noreply.github.com>
2021-08-27 07:32:05 -0300

    Mês de Aniversário (IF ELSE)
    Atividade para praticar estrutura condicional encadeada.
    
    O sistema pede para o usuário digitar o mês de nascimento com dois dígitos;
    Após o usuário digitar, o sistema buscar o mês atribuído aos dígitos e imprime para o usuário a mensagem informando o mês de aniversário.

---
+++ mesDeAniversarioIfElse/mesDeAniversarioIfElse/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Globalization;
+
+namespace contaDeTelefone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1;
+            string mes;
+
+            Console.Write("digite o mês que você nasceu com 2 digitos: ");
+            n1 = int.Parse(Console.ReadLine());
+
+            if (n1 == 1)
+            {
+                mes = "Janeiro";
+            }
+            else if (n1 == 2)
+            {
+                mes = "Fevereiro";
+            }
+            else if (n1 == 3)
+            {
+                mes = "Março";
+            }
+            else if (n1 == 4)
+            {
+                mes = "Abril";
+            }
+            else if (n1 == 5)
+            {
+                mes = "Maio";
+            }
+            else if (n1 == 6)
+            {
+                mes = "Junho";
+            }
+            else if (n1 == 7)
+            {
+                mes = "Julho";
+            }
+            else if (n1 == 8)
+            {
+                mes = "Agosto";
+            }
+            else if (n1 == 9)
+            {
+                mes = "Setembro";
+            }
+            else if (n1 == 10)
+            {
+                mes = "Outrubro";
+            }
+            else if (n1 == 11)
+            {
+                mes = "Novembro";
+            }
+            else if (n1 == 12)
+            {
+                mes = "Dezembro";
+            }
+            else
+            {
+                mes = "Mês inválido";
+            }
+
+            Console.WriteLine("Mês escolhido: " + mes);
+        }
+    }
+}

8c09805e3e8a9fffde15806dd1720cdd90398286
Luã Lafayete Salgado <78837159+LuanLafayete@users.noreply.github.com>
2021-08-27 07:29:20 -0300

    Conta de Telefone
    Uma operadora de telefonia cobre R$ 50,00 por um plano básico que dá direito a 100 minutos de telefone. Cada minuto que exceder a franquia de 100 minutos custa R$ 2,00. Fazer um programa para ler a quantidade de minutos que uma pessoa consumiu, daí mostrar o valor a ser pago.

---
+++ contaDeTelefone/contaDeTelefone/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace contaDeTelefone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int minutos;
+            double contas = 50.00;
+
+            Console.Write("Quantos minutos foram consumidos? ");
+            minutos = int.Parse(Console.ReadLine());
+
+            if (minutos < 100)
+            {
+                Console.Write("VALOR TOTAL = " + contas.ToString("F2", CultureInfo.InvariantCulture));
+            }
+            else
+            {
+                contas = contas + (minutos - 100) * 2.00;
+                Console.WriteLine("VALOR TOTAL = " + contas.ToString("F2", CultureInfo.InvariantCulture));
+            }
+        }
+    }
+}

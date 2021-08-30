427000f15ea51d2510f06cd31c3e890392d62b91
Luã Lafayete Salgado <78837159+LuanLafayete@users.noreply.github.com>
2021-08-30 10:28:19 -0300

    Crescente ou Decrescente
    Leia uma quantidade indeterminada de duplas de valores inteiros X e Y. Escreva para cada X e Y uma mensagem que indique se estes valores foram digitados em ordem crescente ou decrescente.
    
    Entrada
    A entrada contém vários casos de teste. Cada caso contém dois valores inteiros X e Y. A leitura deve ser encerrada ao ser fornecido valores iguais para X e Y.
    
    Saída
    Para cada caso de teste imprima “Crescente”, caso os valores tenham sido digitados na ordem crescente, caso contrário imprima a mensagem “Decrescente”.

---
+++ crescenteOuDecrescente/crescenteOuDecrescente/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+
+namespace crescenteOuDecrescente
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+
+            Console.WriteLine("Digite o primeiro valor");
+            x = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo valor");
+            y = int.Parse(Console.ReadLine());
+
+            while (x != y)
+            {
+                if (x < y)
+                {
+                    Console.WriteLine("Crescente");
+                }
+                else
+                {
+                    Console.WriteLine("Decrescente");
+                }
+
+                Console.WriteLine("Digite o primeiro valor");
+                x = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Digite o segundo valor");
+                y = int.Parse(Console.ReadLine());
+            }
+        }
+    }
+}

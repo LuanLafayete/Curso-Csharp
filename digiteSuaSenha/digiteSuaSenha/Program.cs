9db5e0c63b5abbb1fc8fe8eac90a9650bba7c9f3
Luã Lafayete Salgado <78837159+LuanLafayete@users.noreply.github.com>
2021-08-30 10:56:04 -0300

    Senha Fixa
    Exercício disponível em URI Online Judge.
    https://www.urionlinejudge.com.br/judge/pt/problems/view/1114
    
    Escreva um programa que repita a leitura de uma senha até que ela seja válida. Para cada leitura de senha incorreta informada, escrever a mensagem "Senha Invalida". Quando a senha for informada corretamente deve ser impressa a mensagem "Acesso Permitido" e o algoritmo encerrado. Considere que a senha correta é o valor 2002.

---
+++ digiteSuaSenha/digiteSuaSenha/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace digiteSuaSenha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int senha;
+
+            Console.Write("Digite a sua senha com 4 dígitos: ");
+            senha = int.Parse(Console.ReadLine());
+
+            while (senha != 2002)
+            {
+                Console.WriteLine("Acesso negado");
+                Console.Write("Digite a sua senha com 4 dígitos: ");
+                senha = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Acesso Permitido");
+        }
+    }
+}

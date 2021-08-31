bcef935f31a42a247f38c63a7bd2ab01707a092b
Luã Lafayete Salgado <78837159+LuanLafayete@users.noreply.github.com>
2021-08-31 02:05:52 -0300

    Quadrante do Plano Cartesiano
    Exercício 1115 do URI Online Judge.
    https://www.urionlinejudge.com.br/judge/pt/problems/view/1115
    
    Escreva um programa para ler as coordenadas (X,Y) de uma quantidade indeterminada de pontos no sistema cartesiano. Para cada ponto escrever o quadrante a que ele pertence. O algoritmo será encerrado quando pelo menos uma de duas coordenadas for NULA (nesta situação sem escrever mensagem alguma).
    
    Entrada
    A entrada contém vários casos de teste. Cada caso de teste contém 2 valores inteiros.
    
    Saída
    Para cada caso de teste mostre em qual quadrante do sistema cartesiano se encontra a coordenada lida, conforme o exemplo:
    
    
    Exemplo de Entrada	Exemplo de Saída
    2 2                                   primeiro
    3 -2                                 quarto
    -8 -1                                terceiro
    -7 1                                 segundo
    0 2                                  --

---
+++ quadrantePlanoCartesiano/quadrantePlanoCartesiano/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace quadrantePlanoCartesiano
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o valor de X e Y: ");
+            string[] valores = Console.ReadLine().Split(' ');           
+            int x = int.Parse(valores [0]);
+            int y = int.Parse(valores[1]);
+
+            while (x != 0 && y != 0)
+            {
+                if (x > 0 && y > 0)
+                {
+                    Console.WriteLine("Primeiro Quadrante");
+                }
+                else if (x < 0 && y > 0)
+                {
+                    Console.WriteLine("Segundo Quadrante");
+                }
+                else if (x < 0 && y < 0)
+                {
+                    Console.WriteLine("Terceiro Quadrante");
+                }
+                else
+                {
+                    Console.WriteLine("Quarto Quadrante");
+                }
+                Console.Write("Digite o valor de X e Y: ");
+                valores = Console.ReadLine().Split(' ');
+                x = int.Parse(valores[0]);
+                y = int.Parse(valores[1]);
+            }
+
+            
+
+
+
+        }
+    }
+}

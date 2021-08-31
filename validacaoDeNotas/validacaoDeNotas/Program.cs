f8a0c3bf3eb5b4a4c3cce5299c02031f9d64d28f
Luã Lafayete Salgado <78837159+LuanLafayete@users.noreply.github.com>
2021-08-31 02:31:17 -0300

    Validando as notas dos alunos
    Exercício 1117, disponível em URI ONLINE JUDGE:
    https://www.urionlinejudge.com.br/judge/pt/problems/view/1117
    
    Faça um programa que leia as notas referentes às duas avaliações de um aluno. Calcule e imprima a média semestral. Faça com que o algoritmo só aceite notas válidas (uma nota válida deve pertencer ao intervalo [0,10]). Cada nota deve ser validada separadamente.
    
    Entrada
    A entrada contém vários valores reais, positivos ou negativos. O programa deve ser encerrado quando forem lidas duas notas válidas.
    
    Saída
    Se uma nota inválida  for lida, deve ser impressa a mensagem "nota invalida".
    Quando duas notas válidas forem lidas, deve ser impressa a mensagem "media = " seguido do valor do cálculo. O valor deve ser apresentado com duas casas após o ponto decimal.

---
+++ validacaoDeNotas/validacaoDeNotas/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+namespace validacaoDeNotas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double nota1, nota2, media;
+
+            Console.Write("Digite o resultado da primeira prova: ");
+            nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            while (nota1 < 0 || nota1 > 10)
+            {
+                Console.WriteLine("Nota Inválida");
+                nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+
+            Console.Write("Digite o resultado da segunda prova: ");
+            nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            while (nota2 < 0 || nota2 > 10)
+            {
+                Console.WriteLine("Nota Inválida");
+                nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+
+            media = (nota1 + nota2) / 2;
+
+            Console.WriteLine("A media das provas é: " + media.ToString("F2"), CultureInfo.InvariantCulture);
+        }
+    }
+}

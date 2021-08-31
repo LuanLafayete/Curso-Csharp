f9d31d7933a07e09d2e77d009cada43c957e445f
Luã Lafayete Salgado <78837159+LuanLafayete@users.noreply.github.com>
2021-08-31 03:00:01 -0300

    Tipo de Combustível
    Exercício 1134 proposto no URI ONLINE JUDGE.
    https://www.urionlinejudge.com.br/judge/pt/problems/view/1134
    
    Um Posto de combustíveis deseja determinar qual de seus produtos tem a preferência de seus clientes. Escreva um algoritmo para ler o tipo de combustível abastecido (codificado da seguinte forma: 1.Álcool 2.Gasolina 3.Diesel 4.Fim). Caso o usuário informe um código inválido (fora da faixa de 1 a 4) deve ser solicitado um novo código (até que seja válido). O programa será encerrado quando o código informado for o número 4.
    
    Entrada
    A entrada contém apenas valores inteiros e positivos.
    
    Saída
    Deve ser escrito a mensagem: "MUITO OBRIGADO" e a quantidade de clientes que abasteceram cada tipo de combustível, conforme exemplo.

---
+++ tipoDeCombustivel/tipoDeCombustivel/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace tipoDeCombustivel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int alcool = 0;
+            int gasolina = 0; 
+            int diesel = 0;
+            int numero;
+
+            Console.WriteLine("Qual combustível você usa no seu veículo? ");
+            Console.WriteLine("Digite 1 para Álcool");
+            Console.WriteLine("Digite 2 para Gasolina");
+            Console.WriteLine("Digite 3 para Diesel");
+            Console.WriteLine("Digite 4 para FIM");
+            numero = int.Parse(Console.ReadLine());
+
+            while (numero < 4)
+            {
+                if (numero == 1)
+                {
+                    alcool = alcool + 1;
+                }
+                else if (numero == 2)
+                {
+                    gasolina = gasolina + 1;
+                }
+                else if (numero == 3)
+                {
+                    diesel = diesel + 1;
+                }
+
+                numero = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("MUITO OBRIGADO");
+            Console.WriteLine("Álcool: " + alcool);
+            Console.WriteLine("Gasolina : " + gasolina);
+            Console.WriteLine("Diesel : " + diesel);
+        }
+    }
+}

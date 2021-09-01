8ad24209ad3319aae53e04643cfabef54b1bdcad
Luã Lafayete Salgado <78837159+LuanLafayete@users.noreply.github.com>
2021-08-31 22:13:19 -0300

    Várias Notas com Validação
    Exercício 1118 proposto no URI ONLINE JUDGE
    https://www.urionlinejudge.com.br/judge/pt/problems/view/1118
    
    Escreva um programa para ler as notas da primeira e a segunda avaliação de um aluno. Calcule e imprima a média semestral. O programa só deverá aceitar notas válidas (uma nota válida deve pertencer ao intervalo [0,10]). Cada nota deve ser validada separadamente.
    
    No final deve ser impressa a mensagem “novo calculo (1-sim 2-nao)”, solicitando ao usuário que informe um código (1 ou 2) indicando se ele deseja ou não executar o algoritmo novamente, (aceitar apenas os código 1 ou 2). Se for informado o código 1 deve ser repetida a execução de todo o programa para permitir um novo cálculo, caso contrário o programa deve ser encerrado.
    
    Entrada
    O arquivo de entrada contém vários valores reais, positivos ou negativos. Quando forem lidas duas notas válidas, deve ser lido um valor inteiro X . O programa deve parar quando o valor lido para este X for igual a 2.
    
    Saída
    Se uma nota inválida for lida, deve ser impressa a mensagem “nota invalida”. Quando duas notas válidas forem lidas, deve ser impressa a mensagem “media = ” seguido do valor do cálculo.
    
    Antes da leitura de X deve ser impressa a mensagem "novo calculo (1-sim 2-nao)" e esta mensagem deve ser apresentada novamente se o valor da entrada padrão para X for menor do que 1 ou maior do que 2, conforme o exemplo abaixo.
    
    A média deve ser impressa com dois dígitos após o ponto decimal.

---
+++ variasNotasComValidacao/variasNotasComValidacao/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Globalization;
+
+namespace variasNotasComValidacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double media, nota1, nota2;
+            int escolha;
+
+            Console.WriteLine("Deseja calcular a média?");
+            Console.Write("Digite: 1 para SIM ou 2 para NÃO: ");
+            escolha = int.Parse(Console.ReadLine());
+
+            while (escolha == 1)
+            {                               
+                Console.Write("Digite o resultado da primeira prova: ");
+                nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);               
+                while (nota1 < 0 || nota1 > 10)
+                {
+                    Console.WriteLine("NOTA INVÁLIDA");
+                    Console.Write("Digite o resultado da primeira prova: ");
+                    nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                }
+
+                Console.Write("Digite o resultado da segunda prova: ");
+                nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                while (nota2 < 0 || nota2 > 10)
+                {
+                    Console.WriteLine("NOTA INVÁLIDA");
+                    Console.Write("Digite o resultado da primeira prova: ");
+                    nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                }
+
+                media = (nota1 + nota2) / 2.0;
+                Console.WriteLine("A media das notas é: " + media.ToString("F2", CultureInfo.InvariantCulture));
+
+                Console.WriteLine("Deseja continuar a operação?");
+                Console.Write("Digite 1 para SIM e 2 para NÃO: ");
+                escolha = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("FIM DO PROCESSO");
+        }
+    }
+}

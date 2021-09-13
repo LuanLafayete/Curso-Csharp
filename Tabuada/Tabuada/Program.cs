e8e08264c33f8324a25cc6dc00bee3aaa649d7d6
Luã Lafayete Salgado <78837159+LuanLafayete@users.noreply.github.com>
2021-09-13 18:20:16 -0300

    Tabuada
    Atividade 1078 disponível no URI Online Judge.
    https://www.urionlinejudge.com.br/judge/pt/problems/view/1078
    
    Leia 1 valor inteiro N (2 < N < 1000). A seguir, mostre a tabuada de N:      
    1 x N = N      2 x N = 2N        ...       10 x N = 10N
    
    Entrada
    A entrada contém um valor inteiro N (2 < N < 1000).
    
    Saída
    Imprima a tabuada de N, conforme o exemplo fornecido.

---
+++ Tabuada/Tabuada/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace Tabuada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N, resultado;
+
+            Console.Write("Qual taboada você quer saber: ");
+            N = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++)
+            {
+                resultado = i * N;
+                Console.WriteLine(i + " x " + N + " = " + resultado );
+            }
+        }
+    }
+}

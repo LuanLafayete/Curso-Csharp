38aab9e37381ad36ecebb3d638d8fafbe3cafe54
Luã Lafayete Salgado <78837159+LuanLafayete@users.noreply.github.com>
2021-09-13 19:38:13 -0300

    Conversor de clima Celsius e Fahrenheit
    Pratica da repetição DO / WHILE.
    
    Programa pergunta para o usuário o clima em graus celsius e faz a conversão em fahreheit. 
    Importante ressaltar que o programa pergunta ao usuário se o mesmo deseja repetir ou cancelar a operação.

---
+++ ConversorDeTemperatura/ConversorDeTemperatura/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConversorDeTemperatura
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double celsius, fahrenheit;
+            char repetir;
+
+            do
+            {
+                Console.Write("Digite a temperatura em Celsius: ");
+                celsius = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                fahrenheit = (9.0 * celsius / 5.0) + 32.0;
+                Console.WriteLine("A temperatura em Fahrenheit é: " + fahrenheit.ToString("F1", CultureInfo.InvariantCulture));
+
+                Console.Write("Deseja repetir (s/n)? ");
+                repetir = char.Parse(Console.ReadLine());
+
+            } while (repetir == 's');          
+
+            Console.WriteLine("FIM DO PROGRAMA");
+        }
+    }
+}

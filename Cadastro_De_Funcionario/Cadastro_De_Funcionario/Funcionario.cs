3ba1ca141427dfe3311f1516943067fd8abee277
Luã Lafayete Salgado <78837159+LuanLafayete@users.noreply.github.com>
2021-10-18 21:49:37 -0300

    Cadastro de Funcionários (Class / Object)
    Fazer um programa para ler nome e salário de dois funcionários. Depois, mostrar o salário 
    médio dos funcionários.
    
    Exemplo:
    Dados do primeiro funcionário:
    Nome: Carlos Silva
    Salário: 6300.00
    Dados do segundo funcionário:
    Nome: Ana Marques
    Salário: 6700.00
    Salário médio = 6500.00

---
+++ Cadastro_De_Funcionario/Cadastro_De_Funcionario/Funcionario.cs
@@ -0,0 +1,8 @@
+﻿namespace Cadastro_De_Funcionario
+{
+    class Funcionario
+    {
+        public string nome;
+        public double sal;
+    }
+}
---
+++ Cadastro_De_Funcionario/Cadastro_De_Funcionario/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace Cadastro_De_Funcionario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario a, b;
+            a = new Funcionario();
+            b = new Funcionario();
+
+            Console.WriteLine("Dados do primeiro funcionário: ");
+            Console.Write("Nome: ");
+            a.nome = Console.ReadLine();
+            Console.Write("Salário: ");
+            a.sal = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Dados do segundo funcionário: ");
+            Console.Write("Nome: ");
+            b.nome = Console.ReadLine();
+            Console.Write("Salário: ");
+            b.sal = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double media = (a.sal + b.sal) / 2;
+
+            Console.WriteLine("Salário medio = " + media.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}

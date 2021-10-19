5b703a37289c73bb82d9dec0c2ff3d6e13037cd9
Luã Lafayete Salgado <78837159+LuanLafayete@users.noreply.github.com>
2021-10-19 17:05:56 -0300

    Estoque (Classe / Objeto / Método)
    Fazer um programa para ler os dados de um produto em estoque (nome, preço e quantidade no estoque). 
    Em seguida:
    • Mostrar os dados do produto (nome, preço, quantidade no estoque, valor total no estoque);
    • Realizar uma entrada no estoque e mostrar novamente os dados do produto;
    • Realizar uma saída no estoque e mostrar novamente os dados do produto;
    
    Para resolver este problema, você deve criar uma CLASSE conforme projeto abaixo:
    
    Entre os dados do produto:
    Nome: TV
    Preço: 900.00
    Quantidade no estoque: 10
    Dados do produto: TV, $ 900.00, 10 unidades, Total: $ 9000.00
    
    Digite o número de produtos a ser adicionado ao estoque: 5
    Dados atualizados: TV, $ 900.00, 15 unidades, Total: $ 13500.00
    
    Digite o número de produtos a ser removido do estoque: 3
    Dados atualizados: TV, $ 900.00, 12 unidades, Total: $ 10800.00
    -----------------------------------------------------------------------------------------------------------
    UML
    PRODUTO
    ---------------------------------------------------
    - Nome : string
    - Preco : double
    - Quantidade : int
    ---------------------------------------------------
    + ValorTotalEmEstoque () : double
    + AdicionarProduto (quantidade : int) : void
    + RemoverProduto (quantidade : int) : void

---
+++ Produtos_Em_Estoque/Produtos_Em_Estoque/Produto.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Produtos_Em_Estoque
+{
+    class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public double ValorTotalEmEstoque()
+        {
+            return  Preco * Quantidade;            
+        }
+
+        public void AdicionarProduto(int quantidade)
+        {
+            Quantidade = Quantidade + quantidade;
+        }
+
+        public void RemoverProduto(int quantidade)
+        {
+            Quantidade -= quantidade;
+        }
+
+        public override string ToString()
+        {
+            return Nome
+                + ", $"
+                + Preco.ToString("F2", CultureInfo.InvariantCulture)
+                + ", "
+                + Quantidade
+                + " unidade, Total: $ "
+                + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+    }
+}
---
+++ Produtos_Em_Estoque/Produtos_Em_Estoque/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+
+namespace Produtos_Em_Estoque
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto();
+            
+            Console.WriteLine("Entre os dados do produto: ");
+            Console.Write("Nome: ");
+            p.Nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            p.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidade no estoque: ");
+            p.Quantidade = int.Parse(Console.ReadLine());
+                        
+            Console.WriteLine();
+            Console.Write("Dados do produto: " + p);
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+            int qtd = int.Parse(Console.ReadLine());
+            p.AdicionarProduto(qtd);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + p);
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.Write("Digite o número de produtos a ser removido do estoque: ");
+            qtd = int.Parse(Console.ReadLine());
+            p.RemoverProduto(qtd);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + p);                        
+        }
+    }
+}

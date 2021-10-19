4347dd2f82474e5d02279828836a6300f5b63cd1
Luã Lafayete Salgado <78837159+LuanLafayete@users.noreply.github.com>
2021-10-18 21:17:30 -0300

    Calculo de dois triângulos
    Fazer uma programa para ler as medidas dos lados de dois triângulos X e Y (suponha medidas válidas). E seguida, mostrar o valor das áreas dos dois triângulos e dizer qual dos dois triângulos possui a maior área.
    A fórmula para calcular a área de um triângulo a partir das medidas de seus lados A, B e C é a fórmula de Heron.

---
+++ Lado_de_dois_Triangulos/Lado_de_dois-Triangulos/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+
+namespace Lado_de_dois_Triangulos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo x, y;
+            x = new Triangulo();
+            y = new Triangulo();
+
+            Console.WriteLine("Entre com as medidas do triângulo X: ");
+            x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture); 
+            x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Entre com as medidas do triângulo X: ");
+            y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double pX = (x.A + x.B + x.C) / 2;
+            double pY = (y.A + y.B + y.C) / 2;
+
+            double areaX = Math.Sqrt(pX * (pX - x.A) * (pX - x.B) * (pX - x.C));
+            double areaY = Math.Sqrt(pY * (pY - y.A) * (pY - y.B) * (pY - y.C));
+
+            Console.WriteLine("Área de X = " + areaX.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("Área de Y = " + areaY.ToString("F4", CultureInfo.InvariantCulture));
+
+            if (areaX > areaY)
+            {
+                Console.WriteLine("Maior área: X");
+            }
+            else
+            {
+                Console.WriteLine("Maior área: Y");
+            }
+        }
+    }
+}
---
+++ Lado_de_dois_Triangulos/Lado_de_dois-Triangulos/Triangulo.cs
@@ -0,0 +1,9 @@
+﻿namespace Lado_de_dois_Triangulos
+{
+    class Triangulo
+    {
+        public double A;
+        public double B;
+        public double C;
+    }
+}

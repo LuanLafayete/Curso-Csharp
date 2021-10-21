d1836664ac65f65978c5f94910d1dc03b203129f
Luã Lafayete Salgado <78837159+LuanLafayete@users.noreply.github.com>
2021-10-20 23:41:02 -0300

    Area + Perímetro + Diagonal (Class / Object)
    Fazer um programa para ler os valores da largura e altura de um retângulo. 
    Em seguida, mostrar na tela o valor de sua área, perímetro e diagonal. 
    Usar uma classe como mostrado no projeto abaixo.
    
    Exemplo:
    Entre a largura e altura do retângulo:
    3.00
    4.00
    AREA = 12.00
    PERÍMETRO = 14.00
    DIAGONAL = 5.00
    
    UML
    RETANGULO
    --------------------------------------
    - LARGURA : double
    - ALTURA : double
    ---------------------------------------
    +Area() : double
    +Perimetro() : double
    + Diagonal() : double

---
+++ Area_perimetro_diagonal02/Area_perimetro_diagonal02/Retangulo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Area_perimetro_diagonal02
+{
+    class Retangulo
+    {
+        public double largura;
+        public double altura;
+
+
+        public double Area()
+        {
+            return largura * altura;
+        }
+
+        public double Perimetro()
+        {
+            return 2 * altura + 2 * largura;
+        }
+
+        public double Diagonal()
+        {
+            return Math.Sqrt(Math.Pow(altura, 2.0) + Math.Pow(largura, 2.0));
+        }
+    }    
+}

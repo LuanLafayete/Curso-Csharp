d84bf9bdb2cccc644514d4c196c55e131ce1a63b
Luã Lafayete Salgado <78837159+LuanLafayete@users.noreply.github.com>
2021-10-25 09:14:53 -0300

    Resultado Final das provas
    Fazer um programa para ler o nome de um aluno e as três notas que ele obteve nos três trimestres do ano (primeiro trimestre vale 30 e o segundo e terceiro valem 35 cada). Ao final, mostrar qual a nota final do aluno no ano. 
    Dizer também se o aluno está APROVADO ou REPROVADO e, em caso negativo, quantos pontos faltam 
    para o aluno obter o mínimo para ser aprovado (que é 60 pontos). Você deve criar uma classe Aluno para resolver este problema.
    
    Exemplos:
    Nome do aluno: Alex Green
    Digite as três notas do aluno:
    27.00
    31.00
    32.00
    NOTA FINAL = 90.00
    APROVADO
    
    Nome do aluno: Alex Green
    Digite as três notas do aluno:
    17.00
    20.00
    15.00
    NOTA FINAL = 52.00
    REPROVADO
    FALTARAM 8.00 PONTOS

---
+++ Nota_Dos_Aunos/Nota_Dos_Aunos/Aluno.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Nota_Dos_Aunos
+{
+    class Aluno
+    {
+        public string nome;
+        public double N1;
+        public double N2;
+        public double N3;
+
+        public double NotaFinal()
+        {
+            return N1 + N2 + N3;
+        }       
+       
+    }
+}
---
+++ Nota_Dos_Aunos/Nota_Dos_Aunos/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+
+namespace Nota_Dos_Aunos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno capiroto = new Aluno();
+
+            Console.Write("Nome do aluno: ");
+            capiroto.nome = Console.ReadLine();
+
+            Console.WriteLine("Digite as três notas dos alunos");
+            capiroto.N1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            capiroto.N2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            capiroto.N3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("NOTA FINAL " + capiroto.NotaFinal().ToString("F2", CultureInfo.InvariantCulture));
+                      
+            if (capiroto.NotaFinal() >= 60.00)
+            {
+                Console.WriteLine("APROVADO");
+            }
+            else
+            {
+                Console.WriteLine("REPROVADO");
+                double limite = 60.00 - capiroto.NotaFinal();
+                Console.Write("FALTARAM " + limite.ToString("F2", CultureInfo.InvariantCulture) + " PONTOS");
+            }
+
+        }
+    }
+}

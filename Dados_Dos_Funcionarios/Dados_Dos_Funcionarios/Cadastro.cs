98ff6f3dc04274ea6d010b832d4631febd115dea
Luã Lafayete Salgado <78837159+LuanLafayete@users.noreply.github.com>
2021-10-25 08:21:51 -0300

    Dados dos Funcionários
    Fazer um programa para ler os dados de um funcionário (nome, salário bruto e imposto). 
    Em seguida, mostrar os dados do funcionário (nome e salário líquido). 
    Em seguida, aumentar o salário do funcionário com base em uma porcentagem dada (somente o salário bruto é afetado pela porcentagem) e mostrar novamente os 
    dados do funcionário. 
    Use a classe projetada abaixo.
    
    Exemplo:
    Nome: Joao Silva
    Salário bruto: 6000.00
    Imposto: 1000.00
    Funcionário: Joao Silva, $ 5000.00
    Digite a porcentagem para aumentar o salário: 10.0
    Dados atualizados: Joao Silva, $ 5600.00
    
    
    UML
    Funcionario
    - Nome : sting
    - SalarioBruto : double
    - Imposto : double
    
    --------------------------------------------
    + SalarioLiquido() : double
    + AumentarSalario(porcentagem : double) : void

---
+++ Dados_Dos_Funcionarios/Dados_Dos_Funcionarios/Cadastro.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Dados_Dos_Funcionarios
+{
+    class Cadastro
+    {
+        public string nome;
+        public double Sal;
+        public double Imposto;
+
+        public double Liquido()
+        {
+            return Sal - Imposto;
+        }
+        
+        public void AumentarSalario(double porcentagem)
+        {
+            Sal = Sal + (Sal * porcentagem / 100.0);
+        }
+
+
+
+        public override string ToString()
+        {
+            return "Funcionário: "
+                + nome
+                + ", $"
+                + Liquido();
+
+        }
+    }
+}
---
+++ Dados_Dos_Funcionarios/Dados_Dos_Funcionarios/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+
+namespace Dados_Dos_Funcionarios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cadastro cad = new Cadastro();
+
+            Console.Write("Nome: ");
+            cad.nome = Console.ReadLine();
+
+            Console.Write("Salário Bruto: ");
+            cad.Sal = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            Console.Write("Imposto ");
+            cad.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("");
+            Console.WriteLine(cad);
+
+            Console.WriteLine("");
+            Console.Write("Digite a porcentagem para aumenter o salário: ");
+            double porcentagem = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            cad.AumentarSalario(porcentagem);
+
+            Console.WriteLine("");
+            Console.WriteLine(cad);
+
+
+        }
+    }
+}

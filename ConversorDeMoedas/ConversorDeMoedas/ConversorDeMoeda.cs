27262b7279f809b3dc4922bff5d0a6029c2341ce
Luã Lafayete Salgado <78837159+LuanLafayete@users.noreply.github.com>
2021-10-26 18:38:25 -0300

    Cotação do dólar (static object)
    Faça um programa para ler a cotação do dólar, e depois um valor em dólares a ser comprado por uma pessoa em reais. 
    Informar quantos reais a pessoa vai pagar pelos dólares, considerando ainda que a pessoa terá que pagar 6% de IOF sobre o valor em dólar. 
    Criar uma classe ConversorDeMoeda para ser responsável pelos cálculos.
    
    Exemplo:
    Qual é a cotação do dólar? 3.10
    Quantos dólares você vai comprar? 200.00
    Valor a ser pago em reais = 657.20
    
    Atividade passando por objeto estático

---
+++ ConversorDeMoedas/ConversorDeMoedas/ConversorDeMoeda.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace ConversorDeMoedas
+{
+    class ConversorDeMoeda
+    {
+        public static double IOF = 6.0;
+
+        public static double converter (double quantidade, double cotacao)
+        {
+            double total = cotacao * quantidade;
+            return total + total * IOF / 100.00;
+        }
+    }
+}
---
+++ ConversorDeMoedas/ConversorDeMoedas/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+//Conversor de moeda passando por objeto static.
+
+namespace ConversorDeMoedas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Qual é a cotação do dolar: ");
+            double dolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+           
+            Console.Write("Quantos dólares você vai comprar? ");
+            double quantidade = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double resultado = ConversorDeMoeda.converter(dolar, quantidade);
+
+            Console.WriteLine("Valor a ser pago em reais = " + resultado.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}

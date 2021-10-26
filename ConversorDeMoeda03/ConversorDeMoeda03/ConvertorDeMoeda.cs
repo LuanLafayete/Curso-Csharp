4cbc7e3cd3e72eed147133887003e1e5ffa8bea3
Luã Lafayete Salgado <78837159+LuanLafayete@users.noreply.github.com>
2021-10-26 18:41:04 -0300

    Cotação do dólar (instanciação)
    Faça um programa para ler a cotação do dólar, e depois um valor em dólares a ser comprado por uma pessoa em reais. 
    Informar quantos reais a pessoa vai pagar pelos dólares, considerando ainda que a pessoa terá que pagar 6% de IOF sobre o valor em dólar. 
    Criar uma classe ConversorDeMoeda para ser responsável pelos cálculos.
    
    Exemplo:
    Qual é a cotação do dólar? 3.10
    Quantos dólares você vai comprar? 200.00
    Valor a ser pago em reais = 657.20
    
    Atividade passando por class instanciada

---
+++ ConversorDeMoeda03/ConversorDeMoeda03/ConvertorDeMoeda.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace ConversorDeMoeda03
+{
+    class ConvertorDeMoeda
+    {
+        public double IOF = 6.0;
+
+        public double Converter(double Dolar, double Quantidade)
+        {
+            double total = Dolar * Quantidade;
+            return total + total * IOF / 100;
+        }
+    }
+}
---
+++ ConversorDeMoeda03/ConversorDeMoeda03/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+//Conversor de moeda passando por instanciação.
+
+namespace ConversorDeMoeda03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConvertorDeMoeda conv = new ConvertorDeMoeda();
+
+            Console.Write("Qual é a cotação do dólar? ");
+            double dolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Quantos dólares você vai comprar? ");
+            double quantidade = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double resultado = conv.Converter(dolar, quantidade);
+
+            Console.WriteLine("Valor a ser pago em reais = " + resultado.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}

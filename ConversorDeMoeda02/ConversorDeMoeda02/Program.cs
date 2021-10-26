444fb8f0f428798e5b5653e0367d5867c83a260c
Luã Lafayete Salgado <78837159+LuanLafayete@users.noreply.github.com>
2021-10-26 18:39:46 -0300

    Conversor de moeda (static function)
    Faça um programa para ler a cotação do dólar, e depois um valor em dólares a ser comprado por uma pessoa em reais. 
    Informar quantos reais a pessoa vai pagar pelos dólares, considerando ainda que a pessoa terá que pagar 6% de IOF sobre o valor em dólar. 
    Criar uma classe ConversorDeMoeda para ser responsável pelos cálculos.
    
    Exemplo:
    Qual é a cotação do dólar? 3.10
    Quantos dólares você vai comprar? 200.00
    Valor a ser pago em reais = 657.20
    
    Atividade passando por função como parâmetro

---
+++ ConversorDeMoeda02/ConversorDeMoeda02/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+//Conversor de moeda desenvolvido passando por método dentro da mesma class.
+
+namespace ConversorDeMoeda02
+{
+    class Program
+    {
+        static double IOF = 6.0;
+
+        static void Main(string[] args)
+        {
+            Console.Write("Qual é a cotação do dólar? ");
+            double dolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Quantos dólares você vai comprar? ");
+            double quantidade = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double resultado = Converter(dolar, quantidade);
+
+            Console.WriteLine("Valor a ser pago em reais = " + resultado.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+
+        static double Converter(double Dolar, Double Quantidade)
+        {
+            double total = Dolar * Quantidade;
+            return total + total * IOF / 100;
+        }
+    }
+}

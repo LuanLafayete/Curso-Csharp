348e8f65c1244155bfc8cdadfb2baea60e7081f9
Luã Lafayete Salgado <78837159+LuanLafayete@users.noreply.github.com>
2021-11-16 13:45:36 -0300

    Dados bancários
    Em um banco, para se cadastrar uma conta bancária, é necessário informar o número da conta, o nome do titular da conta, e o valor de depósito inicial que o titular depositou ao abrir a conta. Este valor de depósito inicial, entretanto, é opcional, ou seja: se o titular não tiver dinheiro a depositar no momento de abrir sua conta, o depósito inicial não será feito e o saldo inicial da conta será, naturalmente, zero.
    
    Importante: uma vez que uma conta bancária foi aberta, o número da conta nunca poderá ser alterado. Já o nome do titular pode ser alterado (pois uma pessoa pode mudar de nome por ocasião de casamento, por exemplo).
     
    Por fim, o saldo da conta não pode ser alterado livremente. É preciso haver um mecanismo para proteger isso. O saldo só aumenta por meio de depósitos, e só diminui por meio de saques. Para cada saque realizado, o banco cobra uma taxa de $ 5.00. Nota: a conta pode ficar com saldo negativo se o saldo não for suficiente para realizar o saque e/ou pagar a taxa.
    
    Você deve fazer um programa que realize o cadastro de uma conta, dando opção para que seja ou não informado o valor de depósito inicial. Em seguida, realizar um depósito e depois um saque, sempre mostrando os dados da conta após cada operação. 
    
    Exemplo:
    Entre o número da conta: 8532
    Entre o titular da conta: Alex Green
    Haverá depósito inicial (s/n)? s
    Entre o valor de depósito inicial: 500.00
    Dados da conta:
    Conta 8532, Titular: Alex Green, Saldo: $ 500.00
    Entre um valor para depósito: 200.00
    Dados da conta atualizados:
    Conta 8532, Titular: Alex Green, Saldo: $ 700.00
    Entre um valor para saque: 300.00
    Dados da conta atualizados:
    Conta 8532, Titular: Alex Green, Saldo: $ 395.00
    
    Exemplo 2
    Entre o número da conta: 7801
    Entre o titular da conta: Maria Brown
    Haverá depósito inicial (s/n)? n
    Dados da conta:
    Conta 7801, Titular: Maria Brown, Saldo: $ 0.00
    Entre um valor para depósito: 200.00
    Dados da conta atualizados:
    Conta 7801, Titular: Maria Brown, Saldo: $ 200.00
    Entre um valor para saque: 198.00
    Dados da conta atualizados:
    Conta 7801, Titular: Maria Brown, Saldo: $ -3.00
    
    OBS: Todo o programa foi desenvolvido sem a utilização de Orientação a Objeto.

---
+++ Cadastro-de-banco/Cadastro-de-banco/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Globalization;
+
+namespace Cadastro_de_banco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            //Acesso aos dados do cliente
+            Console.Write("Entre o número da conta: ");
+            int conta = int.Parse(Console.ReadLine());
+
+            Console.Write("Entre o titular da conta: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Haverá depósito inicial (s/n)? ");
+            char seletor = char.Parse(Console.ReadLine());            
+
+            //Laço de repetição para coletar a resposta SIM ou NÃO
+            while (seletor != 's' && seletor != 'n')
+            {
+                Console.WriteLine("Dados incorretos");
+                Console.Write("Haverá depósito inicial (s/n)? ");
+                seletor = char.Parse(Console.ReadLine());
+            }
+
+            //Se o cliente disser SIM
+            if (seletor == 's' || seletor == 'S')
+            {
+                Console.Write("Entre o valor de depósito inicial: ");
+                double valorInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Console.WriteLine();
+
+                Console.WriteLine("Dados da conta:");
+                Console.WriteLine("Conta "
+                    + conta
+                    + ", Titular: "
+                    + nome
+                    + ", Saldo: "
+                    + "$ "
+                    + valorInicial.ToString("F2", CultureInfo.InvariantCulture));
+
+                Console.WriteLine();
+
+                ////Cliente realiza o depósito
+                Console.Write("Entre um valor para depósito: ");
+                double depositoComEntrada = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                double totalComEntrada = valorInicial + depositoComEntrada;
+
+                Console.WriteLine("Dados da conta atualizados:");
+                Console.WriteLine("Conta "
+                    + conta
+                    + ", Titular: "
+                    + nome
+                    + ", Saldo: "
+                    + "$ "
+                    + totalComEntrada.ToString("F2", CultureInfo.InvariantCulture));
+
+                Console.WriteLine();
+
+                //Cliente realiza o saque
+                Console.Write("Entre um valor para saque: ");
+                double saqueComEntrada = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                double financeiro = (totalComEntrada - saqueComEntrada) - 5.00;
+
+                Console.WriteLine("Dados da conta atualizados:");
+                Console.WriteLine("Conta "
+                    + conta
+                    + ", Titular: "
+                    + nome
+                    + ", Saldo: "
+                    + "$ "
+                    + financeiro.ToString("F2", CultureInfo.InvariantCulture));
+            }
+            else
+            {
+                //Se o cliente disser NÃO
+                Console.WriteLine();
+
+                Console.WriteLine("Dados da conta: Conta :");
+                Console.WriteLine("Conta "
+                        + conta
+                        + ", Titular: "
+                        + nome
+                        + ", Saldo: "
+                        + "$ 0.00");
+
+
+                Console.WriteLine();
+
+                //Cliente realiza o depósito
+                Console.Write("Entre um valor para depósito: ");
+                double deposito = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.WriteLine("Dados da conta atualizados: ");
+                Console.WriteLine("Conta "
+                        + conta
+                        + ", Titular: "
+                        + nome
+                        + ", Saldo: "
+                        + "$ "
+                        + deposito.ToString("F2", CultureInfo.InvariantCulture));
+
+                Console.WriteLine();
+
+                //Cliente realiza o saque
+                Console.Write("Entre um valor para saque: ");
+                double saque = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                double total = (deposito - saque) - 5.00;
+
+                Console.WriteLine("Dados da conta atualizados:");
+                Console.WriteLine("Conta "
+                       + conta
+                       + ", Titular: "
+                       + nome
+                       + ", Saldo: "
+                       + "$ "
+                       + total.ToString("F2", CultureInfo.InvariantCulture));
+
+            
+            }
+        }
+    }
+}

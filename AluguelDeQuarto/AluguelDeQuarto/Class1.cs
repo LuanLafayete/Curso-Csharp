eb44072c31a9a8506f201b91b90b05aa45d1aa9f
Luã Lafayete Salgado <78837159+LuanLafayete@users.noreply.github.com>
2022-01-10 17:24:52 -0300

    Aluguel de Quartos
    A dona de um pensionato possui dez quartos para alugar para estudantes, sendo esses quartos identificados pelos números 0 a 9.
    Quando um estudante deseja alugar um quarto, deve-se registrar o nome e email deste estudante.
    Fazer um programa que inicie com todos os dez quartos vazios, e depois leia uma quantidade N representando o número de estudantes que vão alugar quartos (N pode ser de 1 a 10). Em seguida, registre o aluguel dos N estudantes. Para cada registro de aluguel, informar o nome e email do estudante, bem como qual dos quartos ele escolheu de 0 a 9). Suponha que seja escolhido um quarto vago. Ao final, seu programa deve imprimir um relatório de todas ocupações do pensionato, por ordem de quarto, conforme exemplo.
    
    Quantos quartos serão alugados? 3
    Aluguel #1:
    Nome: Maria Green
    Email: maria@gmail.com
    Quarto: 5
    
    Aluguel #2:
    Nome: Marco Antonio
    Email: marco@gmail.com
    Quarto: 1
    
    Aluguel #3:
    Nome: Alex Brown
    Email: alex@gmail.com
    Quarto: 8
    
    Quartos ocupados:
    1: Marco Antonio, marco@gmail.com
    5: Maria Green, maria@gmail.com
    8: Alex Brown, alex@gmail.com

---
+++ AluguelDeQuarto/AluguelDeQuarto/Class1.cs
@@ -0,0 +1,19 @@
+﻿namespace AluguelDeQuarto
+{
+    class Estudantes
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+        public Estudantes(string nome, string email)
+        {
+            Nome = nome;
+            Email = email;
+        }
+
+        public override string ToString()
+        {
+            return Nome + "," + Email;
+        }
+    }
+}
---
+++ AluguelDeQuarto/AluguelDeQuarto/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace AluguelDeQuarto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Estudantes[] vetor = new Estudantes[10];
+
+            Console.Write("Quantos quartos serão alugados? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Aluguel #{i}:");
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Quarto: ");
+                int quarto = int.Parse(Console.ReadLine());
+                vetor[quarto] = new Estudantes(nome, email);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Quantos quartos ocupados: ");
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (vetor[i] != null)
+                {
+                    Console.WriteLine(i + ": " + vetor[i]);
+                }
+            }
+        }
+    }
+}

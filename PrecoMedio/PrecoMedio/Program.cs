872bc43ecae2198c6125bd1d5bf8ebbc3637874d
Luã Lafayete Salgado <78837159+LuanLafayete@users.noreply.github.com>
2022-01-10 13:21:43 -0300

    Valor médio do produto
    Fazer um programa para ler um número inteiro N e os dados (nome e preço) de N Produtos. Armazene os N produtos em um vetor. Em seguida, mostrar o preço médio dos produtos

---
+++ PrecoMedio/PrecoMedio/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+
+namespace PrecoMedio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Quantos produtos serão utilizados? ");
+            int qtd = int.Parse(Console.ReadLine());
+
+            Produto[] vetor = new Produto[qtd];
+
+            for(int i = 0; i < qtd; i++)
+            {
+                Console.WriteLine("Digite o nome do produto");
+                string nome = Console.ReadLine();
+                Console.WriteLine("Digite o preço do produto");
+                double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                vetor[i] = new Produto {Nome = nome, Preco = preco};
+            }
+
+            double soma = 0.0;
+
+            for (int i = 0; i < qtd; i++)
+            {
+                soma += vetor[i].Preco;
+            }
+
+            double media = soma / qtd;
+            Console.WriteLine("AVERAGE PRICE = " + media.ToString("f2", CultureInfo.InvariantCulture));
+        }
+    }
+}

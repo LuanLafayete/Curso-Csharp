d5d267ef8b58105ba74783beeabb7d551beb3a3c
Luã Lafayete Salgado <78837159+LuanLafayete@users.noreply.github.com>
2022-01-11 14:52:49 -0300

    Matriz com Split
    Fazer um programa para ler um número inteiro N e uma matriz de ordem N contendo números inteiros. Em seguida, mostrar a diagonal principal e a quantidade de valores negativos da matriz.

---
+++ MatrizComSplit/MatrizComSplit/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace MatrizComSplit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o tamanha do matriz: ");
+            int ma = int.Parse(Console.ReadLine());
+
+            int[,] mat = new int[ma, ma];
+
+            for (int i = 0; i < ma; i++)
+            {
+                Console.WriteLine("Digite " + ma + " separando por espaço: ");
+                string[] valor = Console.ReadLine().Split(' ');
+                for (int j = 0; j < ma; j++)
+                {
+                    mat[i, j] = int.Parse(valor[j]);
+                }
+            }
+
+            Console.Write("Diagonal Principal: ");
+            for (int i = 0; i < ma; i++ )
+            {
+                Console.Write(mat[i,i] + " ");
+            }
+
+            Console.WriteLine();
+
+            int count = 0;
+            for (int i = 0; i < ma; i++)
+            {
+                for (int j = 0; j < ma; j++)
+                {
+                    if (mat[i,j] < 0)
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            Console.Write("Números Negativos: " + count);
+        }
+    }
+}

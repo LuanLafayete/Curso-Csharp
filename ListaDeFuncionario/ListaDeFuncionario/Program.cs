73cdb1da433acb3b3361d6fe80273976bb6f53c5
Luã Lafayete Salgado <78837159+LuanLafayete@users.noreply.github.com>
2022-01-10 23:38:57 -0300

    Lista de Funcionários
    Fazer um programa para ler um número inteiro N e depois os dados (id, nome e salario) de N funcionários. Não deve haver repetição de id.
    Em seguida, efetuar o aumento de X por cento no salário de um determinado funcionário.
    Para isso, o programa deve ler um id e o valor X. Se o id informado não existir, mostrar uma mensagem e abortar a operação. Ao final, mostrar a listagem atualizada dos funcionários, conforme exemplos.
    Lembre-se de aplicar a técnica de encapsulamento para não permitir que o salário possa ser mudado livremente. Um salário só pode ser aumentado com base em uma operação de aumento por porcentagem dada.
    
    How many employees will be registered? 3
    Emplyoee #1:
    Id: 333
    Name: Maria Brown
    Salary: 4000.00
    
    Emplyoee #2:
    Id: 536
    Name: Alex Grey
    Salary: 3000.00
    
    Emplyoee #3:
    Id: 772
    Name: Bob Green
    Salary: 5000.00
    
    Enter the employee id that will have salary increase : 536
    Enter the percentage: 10.0
    
    Updated list of employees:
    333, Maria Brown, 4000.00
    536, Alex Grey, 3300.00
    772, Bob Green, 5000.00

---
+++ ListaDeFuncionario/ListaDeFuncionario/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+
+namespace ListaDeFuncionario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Quantos funcionários serão cadastrados: ");
+            int qtd = int.Parse(Console.ReadLine());
+
+            List<Funcionario> list = new List<Funcionario>();
+
+            for (int i = 1; i <=qtd; i++)
+            {
+                Console.WriteLine($"Funcionário #{i}: ");
+                Console.Write("ID: ");
+                int id = int.Parse(Console.ReadLine());
+                Console.Write("Nome; ");
+                string nome = Console.ReadLine();
+                Console.Write("Salario: ");
+                double salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                list.Add(new Funcionario(id, nome, salario));
+                Console.WriteLine();
+            }
+
+            Console.Write("Coloque o ID do funcionário que receberá aumento de salário ");
+            int IdAleatorio = int.Parse(Console.ReadLine());
+
+            Funcionario empregado = list.Find(x => x.Id == IdAleatorio);
+            if (empregado != null)
+            {
+                Console.Write("Coloque a porcentagem do aumento: ");
+                double porcentagem = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                empregado.AumentarSalario(porcentagem);
+            }
+            else
+            {
+                Console.WriteLine("Esse ID não existe!" );
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Atualização da lista de funcionários: ");
+            foreach (Funcionario obj in list)
+            {
+                Console.WriteLine(obj);
+            }
+        }
+    }
+}

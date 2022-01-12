9b40b065dfbc45b9c250d46cc47ff65bcb938756
Luã Lafayete Salgado <78837159+LuanLafayete@users.noreply.github.com>
2022-01-12 16:57:12 -0300

    ContratosDeTrabalho
    Ler os dados de um trabalhador com N contratos (N fornecido pelo usuário). Depois, solicitar do usuário um mês e mostrar qual foi o salário do funcionário nesse mês.

---
+++ WorkerContract/WorkerContract/Entities/Department.cs
@@ -0,0 +1,20 @@
+﻿
+namespace WorkerContract.Entities
+{
+    class Department
+    {
+        //Atributos
+        public string Name { get; set; }
+
+        //Construtores
+        public Department()
+        {
+        }
+
+        //Método
+        public Department(string name)
+        {
+            Name = name;
+        }
+    }    
+}
---
+++ WorkerContract/WorkerContract/Entities/HourContract.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace WorkerContract.Entities
+{
+    class HourContract
+    {
+        //Atributos
+        public DateTime Date { get; set; }
+        public Double ValuePerHour { get; set; }
+        public int Hours { get; set; }
+
+        //Construtor
+        public HourContract()
+        {
+        }
+        public HourContract(DateTime date, double valuePerHour, int hours)
+        {
+            Date = date;
+            ValuePerHour = valuePerHour;
+            Hours = hours;
+        }
+
+        //Método
+        public double TotalValue()
+        {
+            return Hours * ValuePerHour;
+        }
+    }
+}

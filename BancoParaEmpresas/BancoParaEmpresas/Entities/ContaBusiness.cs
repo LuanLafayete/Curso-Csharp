de679543af244af1ac03c1f321e883c6b36a2b4b
Luã Lafayete Salgado <78837159+LuanLafayete@users.noreply.github.com>
2022-01-21 09:54:33 -0300

    ContaBusiness com Herença
    Nessa aula aprendemos como utilizar a Herança criando duas classes. 
    1a Class (Conta);
    2a Class (ContaBusiness)
    
    Após criarmos as duas classes passamos por Herança a class Conta para a ContaBusiness e foi adicionado mais 1 atributo e acrescentamos o Construtor.
    
    Para finalizar essa aula, aprendemos a utilizar o Encapsulamento PROTECTED onde pode ser usado somente em uma SUBCLASS e não no programa principal.

---
+++ BancoParaEmpresas/BancoParaEmpresas/Entities/ContaBusiness.cs
@@ -0,0 +1,25 @@
+﻿namespace BancoParaEmpresas.Entities
+{
+    class ContaBusiness : Conta
+    {
+        public double LimiteDeEmprestimo { get; set; }
+
+        public ContaBusiness()
+        {
+        }
+
+        public ContaBusiness(int numero, string usuário, double saldo, double limiteDeEmprestimo) : base (numero, usuário, saldo)
+        {
+            LimiteDeEmprestimo = limiteDeEmprestimo;
+        }
+
+        public void Emprestimo(double quantia)
+        {
+            if (quantia <= LimiteDeEmprestimo)
+            {
+                Saldo += quantia;
+            }            
+        }
+
+    }
+}

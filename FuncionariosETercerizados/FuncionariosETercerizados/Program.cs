897cf8eded9d73ad8a84fb18b9f5a26d9af4ecbe
Luã Lafayete Salgado <78837159+LuanLafayete@users.noreply.github.com>
2022-02-04 14:20:12 -0300

    Funcionários e Terceirizados
    Uma empresa possui funcionários próprios e terceirizados.
    Para cada funcionário, deseja-se registrar nome, horas trabalhadas e valor por hora. 
    Funcionários terceirizados possuem ainda uma despesa adicional.
    O pagamento dos funcionários corresponde ao valor da hora multiplicado pelas horas trabalhadas, sendo que os funcionários terceirizados ainda recebem um bônus correspondente a 110% de sua despesa adicional.
    Fazer um programa para ler os dados de N funcionários (N fornecido pelo usuário) e armazená-los em uma lista. Depois de ler todos os dados, mostrar nome e pagamento de cada funcionário na mesma ordem em que foram digitados.

---
+++ FuncionariosETercerizados/FuncionariosETercerizados/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using FuncionariosETercerizados.Entities;
+
+namespace FuncionariosETercerizados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number of employees: ");
+            int emp = int.Parse(Console.ReadLine());
+
+            List<Employee> list = new List<Employee>();
+
+            for (int i = 1; i <= emp; i++)
+            {
+                Console.WriteLine($"Employee #{i} data");
+                Console.Write("OutSourced (y/n): ");
+                char choice = char.Parse(Console.ReadLine());
+
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+
+                Console.Write("Hours: ");
+                int hours = int.Parse(Console.ReadLine());
+
+                Console.Write("Value per hour: ");
+                double value = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                if (choice == 'y' || choice == 'Y')
+                {
+                    Console.Write("Additional charge: ");
+                    double charge = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    list.Add(new OutSourceEmployee(name, hours, value, charge));
+                }
+                else
+                {
+                    list.Add(new Employee(name, hours, value));
+                }
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("PAYMENTS:");
+
+            foreach(Employee empe in list)
+            {
+                Console.WriteLine(empe.Name + " - $ " + empe.Payment().ToString("F2", CultureInfo.InvariantCulture));
+            }
+
+        }
+    }
+}

6176512f862de6c122fb3dcfc294ca695303d852
Luã Lafayete Salgado <78837159+LuanLafayete@users.noreply.github.com>
2022-06-21 10:30:09 -0300

    Pratica de Herança e Polimorfismo
    Arquivos contendo:
    * Prática de Herança e Polimorfismo;
    * Prática de UpCasting e DownCasting;
    * Operadores AS e IS;
    * Sobreposição;
    * Classes e métodos Selados.

---
+++ PraticaDeHeranca/PraticaDeHeranca/Entities/Account.cs
@@ -0,0 +1,37 @@
+﻿namespace PraticaDeHeranca.Entities
+{
+    class Account
+
+        //ATRIBUTOS
+    {
+        public int Number { get; set; }
+        public string Holder { get; set; }
+        public double Balance { get; set; }
+
+
+        //CONSTRUTORES
+        public Account()
+        {
+        }
+
+        public Account(int number, string holder, double balance)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+        }
+
+        //MÉTODOS
+
+        public virtual void Withdraw (double amount)
+        {
+            Balance -= amount + 5.0;
+        }
+
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+
+    }
+}

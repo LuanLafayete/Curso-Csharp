97d273a6ccb7859c1d9dd88b5b7147b09f8e3009
Luã Lafayete Salgado <78837159+LuanLafayete@users.noreply.github.com>
2022-06-22 13:11:24 -0300

    Pagando Impostos
    Esse projeto foi desenvolvido para praticar CLASSES ABSTRATAS e MÉTODOS ABSTRATOS.
    
    Foi necessário criar primeiramente a classe abstrata e, em seguida, crias as outras duas classes que herdaram da classe abstrata.
    
    OBS: Vale ressaltar que eu tive dificuldade para desenrolar o FOREACH no programa principal e tive que ver o exemplo do professor para entender melhor.

---
+++ PagandoImpostos/PagandoImpostos/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using PagandoImpostos.Entities;
+
+List<TaxPayer> list = new List<TaxPayer>();
+
+Console.Write("Enter the number of tax payers: ");
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= n; i++)
+{
+    Console.WriteLine($"Tax payer #{i} data:");
+
+    Console.Write("Individual or company (i/c)? ");
+    char ch = char.Parse(Console.ReadLine());
+
+    Console.Write("Name: ");
+    string name = Console.ReadLine();
+
+    Console.Write("Anual income: ");
+    double anualIncome = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+    if (ch == 'i' || ch == 'I')
+    {
+        Console.Write("Health expenditures: ");
+        double healthExpenditures = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+        list.Add(new Individual(healthExpenditures, name, anualIncome));
+    }
+    else
+    {
+        Console.Write("Number of employees: ");
+        int numberOfEmployees = int.Parse(Console.ReadLine());
+        list.Add(new Company(numberOfEmployees, name, anualIncome));
+    }
+}
+
+Console.WriteLine();
+
+Console.WriteLine("TAXES PAID:");
+
+double sum = 0;
+
+foreach (TaxPayer payer in list)
+{
+    double tax = payer.Tax();
+    Console.WriteLine(payer.Name + ": $ " + payer.Tax().ToString("F2", CultureInfo.InvariantCulture));
+    sum += tax;
+}
+
+Console.WriteLine();
+Console.WriteLine("TOTAL TAXES: $ " + sum.ToString("F2", CultureInfo.InvariantCulture));

a680ad2b2a64686e5c503b142dda5b1b5b1ad8df
Luã Lafayete Salgado <78837159+LuanLafayete@users.noreply.github.com>
2022-06-24 09:09:32 -0300

    Conta para saque
    Nesse pequeno projeto pratiquei um pouco mais como fazer as exceções dentro da classe ACCOUNT.
    O principal objetivo era lançar as devidas exceções e assim evitar colocá-las no Projeto principal com estruturas aninhadas.
    
    O projeto somente faz a inserção de dados do cliente, coloca um limite de gasto para o mesmo e faz o saque da conta verificando as exceções.

---
+++ ContaParaSaque/ContaParaSaque/Entities/Account.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using ContaParaSaque.Entities.Exceptions;
+
+namespace ContaParaSaque.Entities
+{
+    class Account
+    {
+        public int Numero { get; set; }
+        public string Usuario { get; set; }
+        public double Saldo { get; set; }
+        public double LimiteDeSaque { get; set; }
+
+        public Account()
+        {
+        }
+        
+        public Account(int numero, string usuario, double saldo, double limiteDeSaque)
+        {
+            Numero = numero;
+            Usuario = usuario;
+            Saldo = saldo;
+            LimiteDeSaque = limiteDeSaque;
+        }
+
+        public void Deposit(double amount)
+        {
+            Saldo +=  amount;
+        }
+
+        public void saque(double amount)
+        {
+            // Teste de Saldo insuficiente
+            if (amount > Saldo)
+            {
+                throw new DomainException("Saldo insuficiente");
+            }
+            // Teste de saque acima do Limite de saque
+            if (amount > LimiteDeSaque)
+            {
+                throw new DomainException ("Valor acima do limite de saque ");
+            }          
+            
+            Saldo -= amount;
+        }        
+
+    }
+}
---
+++ ContaParaSaque/ContaParaSaque/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+using ContaParaSaque.Entities.Exceptions;
+using ContaParaSaque.Entities;
+
+
+    Console.WriteLine("Entre com as informações da conta");
+
+    Console.Write("Número da conta: ");
+    int numero = int.Parse(Console.ReadLine());
+
+    Console.Write("Nome do usuário: ");
+    string usuario = Console.ReadLine();
+
+    Console.Write("Saldo inicial: ");
+    double saldo = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+    Console.Write("Limite de Saque: ");
+    double limiteDeSaque = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+    Account Conta = new Account(numero, usuario, saldo, limiteDeSaque);
+
+    Console.WriteLine();
+
+    Console.Write("Entre com o valor para saque: ");
+    double amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+try
+{
+    Conta.saque(amount);
+
+    Console.WriteLine("Novo saldo: " + Conta.Saldo.ToString("F2", CultureInfo.InvariantCulture));
+
+}
+catch (DomainException e)
+{
+    Console.WriteLine("Erro de saque: " + e.Message);
+}
+catch (FormatException e)
+{
+    Console.WriteLine("Erro ao digitar " + e.Message);
+}

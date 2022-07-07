1b4cc0c0aa6db918ba2f9df11ca897ab5bc6beca
Luã Lafayete Salgado <luanlafayete@gmail.com>
2022-07-07 13:17:56 -0300

    FileStream and StreamReader
    Aula prática de FileStream e StreamReader do curso de C# da UDEMY com o professor Nélio Alves

---
+++ FileStreamAndStreamReader/FileStreamAndStreamReader/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+string path = @"C:\Users\luanr\Desktop\Programação\file1.txt";
+
+StreamReader sr = null;
+
+try
+{
+    sr = File.OpenText(path);
+    //string line = sr.ReadToEnd(); OPÇÃPO MENOS VERBOSO
+    while (!sr.EndOfStream)
+    {
+        string line = sr.ReadLine();
+        Console.WriteLine(line);
+    }
+   
+}
+catch (IOException e)
+{
+    Console.WriteLine("An error occured");
+    Console.WriteLine(e.Message);
+}
+finally
+{
+    if (sr != null) sr.Close();   
+}

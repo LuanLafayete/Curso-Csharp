c316a3c7779d24b1972e75706b452f808d23f30b
Luã Lafayete Salgado <luanlafayete@gmail.com>
2022-07-07 13:19:16 -0300

    Bloco Using
    Aula prática de como usar o bloco Using no C#.
    Curso da UDEMY com o professor Nélio Alves

---
+++ BlocoUsing/BlocoUsing/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+string path = @"C:\Users\luanr\Desktop\Programação\file1.txt";
+
+
+try
+{
+
+    using (StreamReader sr = File.OpenText(path))
+    {
+        string line = sr.ReadToEnd();
+        Console.WriteLine(line);
+    }
+
+}
+catch (IOException e)
+{
+    Console.WriteLine("An error occured!");
+    Console.WriteLine(e.Message);
+}

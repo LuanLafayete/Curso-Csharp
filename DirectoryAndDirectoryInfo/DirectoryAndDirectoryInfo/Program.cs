63aa864206e2761238100e9266ea816413382540
Luã Lafayete Salgado <luanlafayete@gmail.com>
2022-07-07 18:31:18 -0300

    Directory And DirectoryInfo
    Aula prática para usar as ferramentas de gerenciamento de pastas em C#.
    Aula pelo curso da UDEMY com o professor Nélio Alves

---
+++ DirectoryAndDirectoryInfo/DirectoryAndDirectoryInfo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+string path = @"C:\Users\luanr\Desktop\Programação\My folder";
+
+try
+{
+    var folders = Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
+    Console.WriteLine("FOLDERS: ");
+    foreach (var s in folders)
+    {        
+        Console.WriteLine(s);
+    }
+
+    Console.WriteLine();
+
+    var files = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories);
+    Console.WriteLine("FILES: ");
+    foreach (var f in files)
+    {        
+        Console.WriteLine(f);
+    }
+
+    Directory.CreateDirectory(path + "\\New Folder");
+}
+catch (IOException e)
+{
+    Console.WriteLine("An error occurred!");
+    Console.WriteLine(e.Message);
+}

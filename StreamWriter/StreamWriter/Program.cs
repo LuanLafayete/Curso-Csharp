574fc7f12d844b1f1c6d514c4aabebaa566cab84
Luã Lafayete Salgado <luanlafayete@gmail.com>
2022-07-07 13:20:49 -0300

    StreamWriter
    Aula prática de como usar o StreamWriter.
    Curso de C# da UDEMY com o professor Nélio Alves

---
+++ StreamWriter/StreamWriter/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+string sourcePath = @"C:\Users\luanr\Desktop\Programação\file1.txt";
+string targetPath = @"C:\Users\luanr\Desktop\Programação\file2.txt";
+
+try
+{
+    string[] lines = File.ReadAllLines(sourcePath);
+    using (StreamWriter sw = File.AppendText(targetPath))
+    {
+        foreach (string line in lines)
+        {
+            sw.WriteLine(line.ToUpper());
+        }
+    }
+}
+catch (IOException e)
+{
+    Console.WriteLine("An error occured!");
+    Console.WriteLine(e.Message);
+}

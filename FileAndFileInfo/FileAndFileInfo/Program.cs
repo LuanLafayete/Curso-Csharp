236f0cb1d562a38420b32a97d705c2f0d8f60438
Luã Lafayete Salgado <luanlafayete@gmail.com>
2022-07-07 13:16:16 -0300

    File and FileInfo
    Aula prática com as ferramentas File e FileInfo no curso da UDEMY com o professor Nélio Alves

---
+++ FileAndFileInfo/FileAndFileInfo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+string sourcePath = @"C:\Users\luanr\Desktop\Programação\file1.txt";
+string targetPath = @"C:\Users\luanr\Desktop\Programação\file2.txt";
+
+try
+{
+    FileInfo fileInfo = new FileInfo(sourcePath);
+    fileInfo.CopyTo(targetPath);
+
+    string[] Lines = File.ReadAllLines(sourcePath);
+    foreach (string txt in Lines)
+    {
+        Console.WriteLine(txt);
+    }
+}
+catch (IOException e)
+{
+    Console.WriteLine("An error occured");
+    Console.WriteLine(e.Message);
+}

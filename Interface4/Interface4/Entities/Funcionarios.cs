5899f2fd5d71e2883635ab436a0add21296d5d9f
Luã Lafayete Salgado <luanlafayete@gmail.com>
2022-07-16 08:45:46 -0300

    Interface4 and IComparable
    Nessa aula foi praticado Interface com IComparable.
    No primeiro instante foi desenvolvido um código para abrir um arquivo em .txt somente com nomes de pessoas.
    Após o código funcionar, foi feito uma alteração no arquivo .txt colocando ao lado do nome a "," e o salário de cada pessoa conforme o exemplo abaixo:
    
    Maria Brown,4300.00
    Alex Green,3100.00
    Bob Grey,3100.00
    Anna White,3500.00
    Alex Black,2450.00
    Eduardo Rose,4390.00
    Willian Red,2900.00
    Marta Blue,6100.00
    Alex Brown,5000.00
    
    Ao fazer isso o programa lançou um erro e quebrou.
    Foi necessário colocar na classe Funcionarios a interface ICompareble e adicionar o método CompareTO para comparar os nomes e colocá-los em ordem alfabética.

---
+++ Interface4/Interface4/Entities/Funcionarios.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+
+
+namespace Interdace4.NovaPasta
+{
+    class Funcionario : IComparable
+    {
+        public string Name { get; set; }
+        public double Salary { get; set; }
+
+        public Funcionario(string cvsFuncionario)
+        {
+            string[] vetor = cvsFuncionario.Split(',');
+            Name = vetor[0];
+            Salary = double.Parse(vetor[1], CultureInfo.InvariantCulture);
+        }
+
+        public override string ToString()
+        {
+            return Name + ", " + Salary.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+        public int CompareTo(object? obj)
+        {
+            if (!(obj is Funcionario))
+            {
+                throw new ArgumentException("Erro de comparação: O argumento não é um funcionário");
+            }
+            Funcionario outro = obj as Funcionario;
+            return Name.CompareTo(outro.Name);
+        }
+    }
+}
---
+++ Interface4/Interface4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+string caminho = @"C:\Users\luanr\Desktop\Programação\01-Curso C Sharp\Interface4\Document.txt";
+
+try
+{
+    using (StreamReader sr = File.OpenText(caminho))
+    {
+        List<string> list = new List<string>();
+        while (!sr.EndOfStream)
+        {
+            list.Add(sr.ReadLine());
+        }
+        list.Sort();
+        foreach (var item in list)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
+catch (IOException e)
+{
+    Console.WriteLine("Error to open the file");
+    Console.WriteLine(e.Message);
+}

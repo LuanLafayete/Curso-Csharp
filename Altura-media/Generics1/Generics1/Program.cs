273efc08d08cdea18eb4a079060b2ba3723e79a6
Luã Lafayete Salgado <luanlafayete@gmail.com>
2022-07-18 16:02:06 -0300

    Continuação Generics
    Dando continuidade à prática de Generics, nessa aula foi praticado sobre importância de parametrizar a classe de forma genérica, e foi construído uma situação teste onde apresentou um problema de Type Safety em tempo de execução.
    
    PROBLEMA:
    No desenvolvimento da classe foi usado uma variável do tipo Object na construção dos métodos;
    No programa principal foi adicionado uma lógica onde solicita ao usuário o preenchimento com string;
    Ainda no programa principal, foi solicitado uma outra lógica que pegas as informações (do tipo string) e solicita um número inteiro.
    
    RESULTADO:
    Pelo fato da classe ser um tipo Object, o programa não reclamou e compilou sem problemas. Ao fazer o teste, o programa lançou uma exceção em tempo de execução.
    
    SOLUÇÂO:
    Foi usado o Generics parametrizando a classe para que no programa principal possa aceitar na mesma classe vários tipos de variáveis usando os métodos da mesma classe.

---
+++ Altura-media/Generics1/Generics1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Generics1.Service;
+
+PrinterService<int> printService = new PrinterService<int>();
+
+Console.Write("How many values? ");
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+    int x = int.Parse(Console.ReadLine());
+    printService.AddValue(x);
+}
+
+int a = printService.First();
+int b = a + 2;
+Console.WriteLine(b);
+
+printService.Print();
+
+Console.WriteLine("First: " + printService.First());
---
+++ Altura-media/Generics1/Generics1/Service/PrinterService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Generics1.Service
+{
+    class PrinterService<T>
+    {
+        private T[] _value = new T[10];
+        private int _count = 0;
+
+        public void AddValue(T value)
+        {
+            if (_count == 10)
+            {
+                throw new InvalidOperationException("PrintService is full");
+            }
+            _value[_count] = value;
+            _count++;
+        }
+
+        public T First()
+        {
+            if (_count == 0)
+            {
+                throw new InvalidOperationException("PrintService is empty");
+            }
+            return _value[0];
+        }
+
+        public void Print()
+        {
+            Console.Write("[");
+            for (int i = 0; i < _count - 1; i++)
+            {
+                Console.Write(_value[i] + ", ");
+            }
+            if (_count > 0)
+            {
+                Console.Write(_value[_count - 1]);
+            }
+            Console.WriteLine("]");
+        }
+    }
+}
